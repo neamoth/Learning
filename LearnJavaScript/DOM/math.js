66be75fa4c28ff88b37ed5c9da1f8487eb4c4511
Neamoth <Neamothcse@gmail.com>
2022-01-17 20:44:13 +0600

    Learn: javaScript-Understanding of Math in javaScript

---
+++ LearnJavaScript/DOM/math.js
@@ -0,0 +1,20 @@
+console.log("Math js practice")
+
+let x = 5;
+let y = 4;
+let z = x+y
+console.log(z);
+
+
+a = Math;
+a = Math.PI;
+a = Math.max(234,53,23,42563);
+a = Math.sqrt(25);
+a = Math.ceil(4.5);
+a = Math.floor(4.5);
+a = Math.abs(-3.6);
+a = Math.random();
+a = 10 +(100-10)*Math.random();
+a = Math.ceil(10 +(100-10)*Math.random());
+a = Math.ceil(-10 +(-100+10)*Math.random());
+console.log(a);
