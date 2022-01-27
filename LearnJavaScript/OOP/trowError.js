becc8654b9c88288317f4ee3f775563bd86d5421
Neamoth <Neamothcse@gmail.com>
2022-01-27 10:40:48 +0600

    learn: javaScipt- Try catch error handling

---
+++ LearnJavaScript/OOP/trowError.js
@@ -0,0 +1,18 @@
+console.log('Error Handling And Try Catch');
+
+
+let a = "neamoth ullah";
+if (a !=undefined){
+    // throw new console.error("this is not undefined");
+}else{
+    console.log('This is undefined');
+}
+
+try {
+    // dskfjadsfjaslf
+    console.log('We are inside the try block');
+    functionNeamoth();
+} catch (error) {
+    console.log('Are you ok?');
+    console.log(error);
+}
